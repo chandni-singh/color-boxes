d5c4ecee80ef4da46e1264d687a7e730d0845ebb
Chandni <chandni1141996@gmail.com>
2021-04-05 15:12:06 +0530

    starter records

---
+++ src/ColorBox.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ColorBox extends Component {
+    render() {
+        return(
+            <h1>Hello from ColorBox!</h1>
+        )
+    }
+}
+
+export default ColorBox;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import ColorBox from './ColorBox';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends Component {
+  render() {
+    return (
+      <ColorBox />
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
